0223d2b039b0d5283784e6d690b6b4e377c97e2d
brunoara21 <bruno.rebaza@smoothstack.com>
2021-08-26 17:14:04 -0400

    Initial Spring commit

---
+++ src/main/java/com/ss/scrumptious_drivers/ScrumptiousDriversServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ss.scrumptious_drivers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumptiousDriversServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrumptiousDriversServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ss/scrumptious_drivers/ScrumptiousDriversServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ss.scrumptious_drivers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrumptiousDriversServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
